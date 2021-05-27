ec1f5d9687c611c3f75446db0f6238df69635f5d
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-05-27 14:35:48 -0400

    update

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,19 @@ const userSchema = new mongoose.Schema({
         required: [true, 'please enter a password'],
         minlength: [6, 'minimum password length is 6 characters']
     },
+});
+
+//fire a function after doc saved to db
+userSchema.post('save', async function (doc, next) {
+const salt = await bcrypt.genSalt();
+this.password = await bcrypt.hash(this.password, salt);
+next();
+});
+
+//fire a function before doc saved to db
+userSchema.pre('save', function (next) {
+    console.log('user about to be created and saved', this);
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
