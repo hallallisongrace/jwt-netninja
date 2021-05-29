708c522fc83d8c8d4d72d42bb41a58f5ff3d22aa
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-05-29 10:10:01 -0400

    update

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://hallallisongrace:<mongo1231!>@cluster0.oh7hl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://hallallisongrace:mongo123!@cluster0.oh7hl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
